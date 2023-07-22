96075056e81e0e45ae64d1f842b62d988e87476f
Prakhar Singh <prakharsingh2001@hmail.com>
2023-07-22 10:00:46 +0530

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,122 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:social_media_app/components/text_field.dart';
+import 'package:social_media_app/components/wall_post.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //user
+  final currentUser = FirebaseAuth.instance.currentUser!;
+
+  //text controller
+  final textController = TextEditingController();
+
+  //sign user out
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  //post message
+  void postMessage() {
+    //only post when there is something in the text field
+    if (textController.text.isNotEmpty) {
+      //store in firebase
+      FirebaseFirestore.instance.collection("User Posts").add({
+        'UserEmail': currentUser.email,
+        'Message': textController.text,
+        'TimeStamp': Timestamp.now(),
+      });
+    }
+
+    //clear textfield
+    setState(() {
+      textController.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: const Center(child: Text("ABC")),
+        backgroundColor: Colors.grey[900],
+        actions: [
+          //sign out button
+          IconButton(onPressed: signOut, icon: const Icon(Icons.logout))
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            //the wall
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("User Posts")
+                    .orderBy(
+                      "TimeStamp",
+                      descending: false,
+                    )
+                    .snapshots(),
+                builder: (context, snapshots) {
+                  if (snapshots.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshots.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        //get the messages
+                        final post = snapshots.data!.docs[index];
+                        return WallPost(
+                          message: post['Message'],
+                          user: post['UserEmail'],
+                        );
+                      },
+                    );
+                  } else if (snapshots.hasError) {
+                    return Center(
+                      child: Text('Error${snapshots.error}'),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+
+            //post message
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MyTextField(
+                      controller: textController,
+                      hintText: "Write something ",
+                      obscureText: false,
+                    ),
+                  ),
+                  //post button
+                  IconButton(
+                    onPressed: postMessage,
+                    icon: const Icon(Icons.arrow_circle_right_outlined),
+                  ),
+                ],
+              ),
+            ),
+
+            //logged in as
+            Text("Loged in as: ${currentUser.email!}"),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,126 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:social_media_app/components/button.dart';
+import 'package:social_media_app/components/text_field.dart';
+
+class LoginPage extends StatefulWidget {
+  final Function()? onTap;
+  const LoginPage({super.key, this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+
+  //Sign in method
+  void signIn() async {
+    //show loading circle
+    showDialog(
+      context: context,
+      builder: (context) => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailTextController.text,
+          password: passwordTextController.text);
+      //pop loadin circle
+      if (context.mounted) Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      //pop loading circle
+      Navigator.pop(context);
+      //display error message
+      displayMessage(e.code);
+    }
+  }
+
+  //display a dialog message
+  void displayMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(message),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //logo
+                const Icon(
+                  Icons.abc,
+                  size: 150,
+                ),
+
+                //welcome back message
+                Text(
+                  "Welcome back, you've been missed!",
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(height: 25),
+                //email textfield
+                MyTextField(
+                  controller: emailTextController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 15),
+                //password
+                MyTextField(
+                  controller: passwordTextController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 25),
+                //sign in button
+                MyButton(
+                  text: 'Log In',
+                  onTap: signIn,
+                ),
+                const SizedBox(height: 25),
+                //go to register page
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Not a Member?',
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        'Sign Up now',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
